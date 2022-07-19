d48b650a3dbce2c0f2bc14b5f57503b8a566f16f
Abhishek-Singh02 <abhishek9502as@gmail.com>
2022-07-19 14:18:31 +0530

    Carousel Fixed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,30 @@
 import React from "react";
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 import Header from "../Header/Header";
 import WeatherInfo from "../Weather/WeatherInfo";
 import Info from "../Info/Info";
 import Dayscarousel from "../Dayscarousel/Dayscarousel";
 import { DataProvider } from "../../weatherData";
 
-
 // import styles from "./App.module.css"
 
 const App = () => {
-    return (
-      <DataProvider>
-        <div className={styles.container}>
-          <div id="container">
-            <Header />
-            <div className={styles.main}>
-              <WeatherInfo />
-              <Info />
-            </div>
-            <div>
-              <Dayscarousel />
-            </div>
+  return (
+    <DataProvider>
+      <div className={styles.container}>
+        <div id="container">
+          <Header />
+          <div className={styles.main}>
+            <WeatherInfo />
+            <Info />
+          </div>
+          <div id="day">
+            <Dayscarousel />
           </div>
         </div>
-      </DataProvider>
-    );
+      </div>
+    </DataProvider>
+  );
 };
 
 export default App;
